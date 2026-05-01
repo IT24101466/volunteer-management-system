070ad7dad13345cf7d5b6f8384874c8d78c5a125
IT24101466 <it24101466@my.sliit.lk>
2026-05-01 20:20:54 +0530

    Admin Updates and Notification Feature

---
+++ backend/models/Notification.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const notificationSchema = new mongoose.Schema({
+  recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  type: {
+    type: String,
+    enum: [
+      'application_status', 'new_application',
+      'donation_status', 'donation_received',
+      'contribution_received', 'contribution_status',
+      'comment_reply', 'comment_like', 'follow_new_opportunity',
+      'opportunity_comment', 'opportunity_vote',
+      'publisher_review', 'publisher_rating', 'publisher_vote'
+    ],
+    required: true
+  },
+  message: { type: String, required: true },
+  relatedId: { type: mongoose.Schema.Types.ObjectId, default: null },
+  relatedType: { type: String, default: '' },
+  read: { type: Boolean, default: false }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Notification', notificationSchema);
